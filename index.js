86f28bcd1a4aaf902ef0b304bd5cf3e2c4f08428
TTHledieu <theotime.huart@epitech.eu>
2018-02-16 16:32:05 +0100

    Adding material-ui-next and styling header

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { MuiThemeProvider } from 'material-ui/styles';
+import theme from 'utils/colors';
 import store from 'utils/store';
 import Root from 'containers/Views/Root';
 
@@ -10,7 +12,9 @@ import Root from 'containers/Views/Root';
 
 render(
   <Provider store={store}>
-    <Root />
+    <MuiThemeProvider theme={theme}>
+      <Root />
+    </MuiThemeProvider>
   </Provider>,
   // $FlowFixMe
   document.getElementById('app'),
--- src/containers/Views/Header/index.js
+++ src/containers/Views/Header/index.js
@@ -1,25 +1,52 @@
 // @flow
 
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import Typography from 'material-ui/Typography';
+import { withTheme } from 'material-ui/styles';
 import styles from './Header.css';
 
 type Props = {
 
 };
 
-type State = {
-
-}
-
 // eslint-disable-next-line react/prefer-stateless-function
-class Header extends React.Component<Props, State> {
+class Header extends React.Component<Props> {
+  goalText: string = 'Visual interpretation of what happen in a blockchain';
+
   render() {
     return (
       <div className={styles.container}>
-        <p>Headerbar</p>
+        <div className={styles.search}>
+          <TextField
+            id="search"
+            label="Adress / Block / Txhash"
+            fullWidth
+            onChange={event => console.log(event.target.value)}
+          />
+        </div>
+        <div className={styles.title}>
+          <Typography
+            align="center"
+            variant="display2"
+            color="secondary"
+          >
+            seeblock
+          </Typography>
+        </div>
+        <div className={styles.goal}>
+          <Typography
+            align="right"
+            variant="subheading"
+            noWrap
+            color="primary"
+          >
+            {this.goalText}
+          </Typography>
+        </div>
       </div>
     );
   }
 }
 
-export default Header;
+export default withTheme()(Header);
