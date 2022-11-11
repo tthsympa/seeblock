7edc483f109aab85427a1d4ab42699b0ad9fd232
TTH <thuart@scaleway.com>
2022-11-11 23:28:36 +0100

    chore: update release

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,11 @@
-FROM node:14.4-stretch as builder
+FROM node:16-alpine as builder
 
-# ARG PORT
-
-# Create app directory
 WORKDIR /usr/src/app
 
-# Install app dependencies
 COPY package.json yarn.lock ./
-
 RUN yarn --pure-lockfile
 
-# Bundle app source
 COPY . .
-
 RUN yarn build
 
 EXPOSE 8080
--- src/sections/Warning/index.js
+++ src/sections/Warning/index.js
@@ -15,15 +15,19 @@ class Header extends React.Component<Props, State> {
   render() {
     return (
       <div className={styles.container}>
-        <Typography variant="h4" color="primary" style={{ marginBottom: '8px'}}>
-          Oops ! You seems not connected to the Ethereum network
+        <Typography
+          variant="h4"
+          color="primary"
+          style={{ marginBottom: '8px' }}
+        >
+          Oops ! We can't connect to the Ethereum network
         </Typography>
         <Typography variant="body1" color="secondary">
-          In ordre to <i>seeblock</i> to work, a Ethereum blockchain provider
-          must be injected in your browser.
+          For <i>seeblock</i> to work, an Ethereum provider must be injected in
+          your browser.
         </Typography>
         <Typography variant="body1" color="secondary">
-          An easy method is to install{' '}
+          You can install{' '}
           <Link
             href="https://metamask.io/"
             target="_blank"
@@ -33,11 +37,7 @@ class Header extends React.Component<Props, State> {
           >
             Metamask
           </Link>{' '}
-          web extansion in your browser (Chrome, Firefox)
-        </Typography>
-        <Typography variant="body1" color="secondary">
-          It'll allow you to interact with the Ethereum network. <i>seeblock</i>{' '}
-          will use it perform it's requests.
+          web extension in your browser (Chrome, Firefox)
         </Typography>
       </div>
     )
